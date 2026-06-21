db10a9781afe163de218f2fea44e63a55855c752
OperonSistema <operonsistema@gmail.com>
2026-06-21 14:43:34 -0300

    feat(pwa): rota /agenda/:agendamentoId/os/:osId

--- frontend-instalador/src/App.jsx
+++ frontend-instalador/src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Agenda from "./pages/Agenda";
 import AgendamentoDetalhe from "./pages/AgendamentoDetalhe";
+import FichaTecnicaInstalador from "./pages/FichaTecnicaInstalador";
 import Rotas from "./pages/Rotas";
 import Abastecimento from "./pages/Abastecimento";
 import Perfil from "./pages/Perfil";
@@ -27,6 +28,7 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route path="/agenda" element={<Agenda />} />
             <Route path="/agenda/:id" element={<AgendamentoDetalhe />} />
+            <Route path="/agenda/:agendamentoId/os/:osId" element={<FichaTecnicaInstalador />} />
             <Route path="/rotas" element={<Rotas />} />
             <Route path="/abastecimento" element={<Abastecimento />} />
             <Route path="/perfil" element={<Perfil />} />
